55c401e092b35c2790d3341f3526fcef23fc7e91
Jonatas Sas <jonatas@jsas.dev>
2026-03-20 08:21:34 -0300

    fix: resolve critical neovim config issues and add versioning
    Neovim fixes:
    - Remove 5 non-existent plugin entries from init.lua (telescope-*, nvim-cmp)
    - Remove SessionRestore button from alpha (no session plugin installed)
    - Remove 6 Trouble keymaps from keymaps.lua (plugin not installed)
    - Add missing return to conform.nvim format_on_save callback
    
    Architecture support:
    - Detect uname -m and set correct Neovim download URL (x86_64/arm64)
    - Detect architecture for fnm download in install and upgrade-node-manager
    
    Versioning:
    - Add OPSCONFIG_VERSION="3.0.0" to all scripts
    - Add --version flag to install and update scripts
    - Add --help flag to install script

--- nvim/.config/nvim/lua/opsconfig/core/keymaps.lua
+++ nvim/.config/nvim/lua/opsconfig/core/keymaps.lua
@@ -666,57 +666,6 @@ local keymaps = function()
       enabled = true,
     },
 
-    -- SUBSECTION: Trouble
-    -- Plugin Configuration File: ../plugins/development/trouble.lua
-    {
-      modes = 'n',
-      lhs = '<leader>xx',
-      rhs = '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-    {
-      modes = 'n',
-      lhs = '<leader>xX',
-      rhs = '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-      desc = 'Buffer Diagnostics (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-    {
-      modes = 'n',
-      lhs = '<leader>cs',
-      rhs = '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = 'Symbols (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-    {
-      modes = 'n',
-      lhs = '<leader>cl',
-      rhs = '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-    {
-      modes = 'n',
-      lhs = '<leader>xL',
-      rhs = '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-    {
-      modes = 'n',
-      lhs = '<leader>xQ',
-      rhs = '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
-      opts = {},
-      enabled = true,
-    },
-
     -- SUBSECTION: Clipboard (path copying)
     {
       modes = 'n',
